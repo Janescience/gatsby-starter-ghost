413c47ac181ea09f326937facd8e73ba7aa297a0
janwit.si <janwit.si@ocean.co.th>
2022-02-24 19:08:33 +0700

    Fix in MetaData

--- src/components/common/meta/MetaData.js
+++ src/components/common/meta/MetaData.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import url from 'url'
+import { url } from 'url'
 
 import config from '../../../utils/siteConfig'
 import ArticleMeta from './ArticleMeta'
